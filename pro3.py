0c2404770b87c8d392e8479b8892c936909c591d
aj <ajcs24ai@gmail.com>
2024-08-08 14:46:44 +0530

    grouping  pandas dataframe

--- pro3.py
+++ pro3.py
@@ -2,5 +2,5 @@ import pandas as pd
 t={'course':["python","java","dbms","mma","cn"],'fee':[300,600,21,350,67],'complexity':[100,56,32,10,67]}
 d=pd.DataFrame(t)
 print(d)
-print("\n",d.pivot(columns='course',values='complexity'))
-print("\n",d.melt())
+c=d.groupby('course').agg({'fee':'min'})
+print("\n",c)
